875daf259b2843fa7512d07dcefda4653ae28c28
NoelHa28 <n.d.r.haarman@student.rug.nl>
2024-04-14 20:32:51 +0000

    start from assignment8

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,3 +1,97 @@
 package is.hi.hbv202g.assignment8;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibrarySystem {
+    private List<User> users;
+    private List<Book> books;
+    private List<Lending> lendings;
+
+    public LibrarySystem() {
+        this.users = new ArrayList<>();
+        this.books = new ArrayList<>();
+        this.lendings = new ArrayList<>();
+    }
+
+    public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName) {
+        this.books.add(new Book(title, authorName));
+    }
+
+    public void addBookWithTitleAndAuthorList(String title, List<Author> authors) throws EmptyAuthorListException {
+        this.books.add(new Book(title, authors));
+    }
+
+    public void addStudentUser(String name, boolean feePaid) {
+        this.users.add(new Student(name, feePaid));
+    }
+
+    public void addFacultyMemberUser(String name, String department) {
+        this.users.add(new FacultyMember(name, department));
+    }
+
+    public Book findBookByTitle(String title) throws UserOrBookDoesNotExistException {
+        for (Book book : this.books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        throw new UserOrBookDoesNotExistException("Book with title " + title + " does not exist");
+    }
+
+    public User findUserByName(String name) throws UserOrBookDoesNotExistException {
+        for (User user : this.users) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
+        }
+        throw new UserOrBookDoesNotExistException("User with name " + name + " does not exist");
+    }
+
+    public void borrowBook(User user, Book book) throws UserOrBookDoesNotExistException  {
+        User foundUser = findUserByName(user.getName());
+        Book foundBook = findBookByTitle(book.getTitle());
+    
+        this.lendings.add(new Lending(foundBook, foundUser));
+    }
+
+    public void extendLending(User andri, Book book, LocalDate newDueDate) {
+        for (Lending lending : this.lendings) {
+            if (lending.getBook().equals(book) && lending.getUser().equals(andri)) {
+                lending.setDueDate(newDueDate);
+                break;
+            }
+        }
+    }
+
+    public void returnBook(User user, Book book) throws UserOrBookDoesNotExistException {
+        Lending foundLending = null;
+        for (Lending lending : this.lendings) {
+            if (lending.getBook().equals(book) && lending.getUser().equals(user)) {
+                foundLending = lending;
+                break;
+            }
+        }
+        if (foundLending == null) {
+            throw new UserOrBookDoesNotExistException("Book was not borrowed by this user");
+        }
+        this.lendings.remove(foundLending);
+    }
+    
+    public void printLendings(User user) {
+        boolean hasLendings = false;
+        for (Lending lending : this.lendings) {
+            if (lending.getUser().equals(user)) {
+                System.out.println(lending.getBook().getTitle() +
+                        " is currently borrowed by " +
+                        lending.getUser().getName() +
+                        " until " + lending.getDueDate());
+                hasLendings = true;
+            }
+        }
+        if (!hasLendings) {
+            System.out.println(user.getName() + " has no lendings");
+        }
+    }
 }
--- src/main/java/is/hi/hbv202g/assignment8/Main.java
+++ src/main/java/is/hi/hbv202g/assignment8/Main.java
@@ -1,13 +1,39 @@
 package is.hi.hbv202g.assignment8;
 
-/**
- * Hello world!
- *
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+/*
+ * This main function shows some abilities of the library system class
+ * 
  */
 public class Main
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws EmptyAuthorListException, UserOrBookDoesNotExistException
     {
         LibrarySystem myLibrarySystem = new LibrarySystem();
+        myLibrarySystem.addStudentUser("Noëlla Hilgen", true);
+        myLibrarySystem.addFacultyMemberUser("Andri Gudjohnsen", "Computer Science");
+        myLibrarySystem.addBookWithTitleAndNameOfSingleAuthor("Harry Potter and the Philosopher's Stone",
+                "J.K. Rowling");
+        List<Author> authorsbook2 = new ArrayList<>();
+        authorsbook2.add(new Author("Thomas H. Cormen"));
+        authorsbook2.add(new Author("Charles E. Leiserson"));
+        authorsbook2.add(new Author("Ronald L. Rivest"));
+        authorsbook2.add(new Author("Clifford Stein"));
+        myLibrarySystem.addBookWithTitleAndAuthorList("Introduction to Algorithms", authorsbook2);
+        User Noella = myLibrarySystem.findUserByName("Noëlla Hilgen");
+        User Andri = myLibrarySystem.findUserByName("Andri Gudjohnsen");
+        Book HarryP = myLibrarySystem.findBookByTitle("Harry Potter and the Philosopher's Stone");
+        Book Intro = myLibrarySystem.findBookByTitle("Introduction to Algorithms");
+        myLibrarySystem.borrowBook(Noella, HarryP);
+        myLibrarySystem.borrowBook(Andri, Intro);
+        myLibrarySystem.printLendings(Noella);
+        myLibrarySystem.printLendings(Andri);
+        myLibrarySystem.extendLending((FacultyMember) Andri, Intro, LocalDate.now().plusDays(60));
+        myLibrarySystem.returnBook(Noella, HarryP);
+        myLibrarySystem.printLendings(Noella);
+        myLibrarySystem.printLendings(Andri);
     }
 }
