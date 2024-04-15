6e0f61f84c1da9c2355088c283f7ea675855106e
NoelHa28 <n.d.r.haarman@student.rug.nl>
2024-04-15 23:18:50 +0000

    Added author and book tests

---
+++ src/test/java/is/hi/hbv202g/assignment8/AuthorTest.java
@@ -0,0 +1,25 @@
+package is.hi.hbv202g.assignment8;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AuthorTest {
+    private Author author;
+
+    @Before
+    public void setUp() {
+        author = new Author("Dan Brown");
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Dan Brown", author.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        author.setName("J.R.R. Tolkien");
+        assertEquals("J.R.R. Tolkien", author.getName());
+    }
+}
